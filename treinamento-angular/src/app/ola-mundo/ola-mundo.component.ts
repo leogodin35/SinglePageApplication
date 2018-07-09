fb82253d9f64731ff4f155a7b7bc5e85833b22f9
Leonardo Godinho <godinho.leo@gmail.com>
2018-07-09 17:36:46 -0300

    inclusão do bootstrap

--- treinamento-angular/src/app/ola-mundo/ola-mundo.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector:'ola-mundo', // seletor <ola-mundo></ola-mundo>
-    templateUrl:'./ola-mundo.component.html',
-    styleUrls:['./ola-mundo.component.css']
-})
-export class OlaMundoComponent{} // lembrar de exportar a classe
