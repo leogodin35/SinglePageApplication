f24c5591e7c49edaed44351ab03c916de969c135
Leonardo Godinho <godinho.leo@gmail.com>
2018-07-09 16:52:57 -0300

    compomente ola mundo

---
+++ treinamento-angular/src/app/ola-mundo/ola-mundo.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'ola-mundo', // seletor <ola-mundo></ola-mundo>
+    templateUrl:'./ola-mundo.component.html',
+    styleUrls:['./ola-mundo.component.css']
+})
+export class OlaMundoComponent{} // lembrar de exportar a classe
