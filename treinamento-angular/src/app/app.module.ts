59cb775a653e03c011c99f43d7fd44928523e929
Leonardo Godinho <godinho.leo@gmail.com>
2018-08-11 18:08:41 -0300

    aula 14

--- treinamento-angular/src/app/app.module.ts
+++ treinamento-angular/src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
 import { PainelSimplesComponent } from './painel-simples/painel-simples.component';
 import { BarraNavegacaoComponent } from './barra-navegacao/barra-navegacao.component';
+import { MenuLateralComponent } from './barra-navegacao/menu-lateral/menu-lateral.component';
+import { ResumoComponent } from './resumo/resumo.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     PainelSimplesComponent,
-    BarraNavegacaoComponent
+    BarraNavegacaoComponent,
+    MenuLateralComponent,
+    ResumoComponent
   ],
   imports: [
     BrowserModule
